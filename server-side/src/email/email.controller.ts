d44675786bbb1c666825b19842435de683181852
unikorm <adaled00@gmail.com>
2024-08-08 23:45:59 +0200

    add dtos

--- server-side/src/email/email.controller.ts
+++ server-side/src/email/email.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { EmailService } from './email.service';
+import { send } from 'process';
+import { SendEmailDto } from './dtos';
 
 @Controller('email_sending')
 export class EmailController {
   constructor(private readonly emailService: EmailService) {}
 
   @Post()
-  async sendEmail(@Body() body: { message: string }) {
-    const { message } = body;
-    return await this.emailService.sendEmail(message);
+  async sendEmail(@Body() sendEmailDto: SendEmailDto) {
+    return await this.emailService.sendEmail(sendEmailDto);
   }
 }
--- server-side/src/email/email.service.ts
+++ server-side/src/email/email.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
+import { SendEmailDto } from './dtos';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
-  constructor(private readonly mailService: MailerService) {}
+  constructor(
+    private readonly mailService: MailerService,
+    private readonly cS: ConfigService,
+  ) {}
 
-  sendEmail = async (message: string) => {
+  sendEmail = async (sendEmailDto: SendEmailDto) => {
+    const { name, email, phone, message } = sendEmailDto;
     try {
       await this.mailService.sendMail({
-        to: 'adaled00@gmail.com',
+        to: this.cS.get<string>('EMAIL_USERNAME'),
         subject: 'Test Subject',
         text: message,
       });
