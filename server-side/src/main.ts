dc7583db6eb977a9922d7ba46815297760f9864b
unikorm <adaled00@gmail.com>
2024-08-09 23:44:08 +0200

    ..

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:4200', // Replace with your Angular app's URL
+  });
   await app.listen(3000);
 }
 bootstrap();
