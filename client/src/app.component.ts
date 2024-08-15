39b507060e18afc477b6fbe2822cde1b802cfb7a
unikorm <adaled00@gmail.com>
2024-08-15 22:48:17 +0200

    style of menu is dd, it works as i want now

--- client/src/app.component.ts
+++ client/src/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MainMenuComponent } from './components/main-menu/main-menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MainMenuComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
