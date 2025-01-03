68f26c195a9748333393c4c09ea97ca094a8e8d0
adamko <adaled00@gmail.com>
2025-01-03 18:49:39 +0100

    feat(error-page): start not found page, details

--- client/src/pages/not-found/not-found.component.ts
+++ client/src/pages/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'not-found',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
 })
