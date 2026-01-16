5893db42941029d72e7051f61ee28348599e8a58
adamko <adaled00@gmail.com>
2026-01-16 23:42:34 +0100

    feat(PROD deploy): details, remove cors settings in BE and modify htaccess to handle local font file

--- server-side/src/main.ts
+++ server-side/src/main.ts
@@ -8,11 +8,6 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
 
   const port = configService.get('PORT', 3000);
-  const corsOrigin = configService.get('CORS_ORIGIN'); // for local purpose only, browser enforce CORS -> on prod it handle nginx
-
-  app.enableCors({
-    origin: corsOrigin,
-  });
   await app.listen(port);
   Logger.log(`🚀 server running on: http://localhost:${port}`);
 }
